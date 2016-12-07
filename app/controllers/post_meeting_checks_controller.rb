738c7ddac494bd19cd08ff97e621972803718da1
Raghu Betina <raghu@firstdraft.com>
2016-12-07 16:15:33 +0000

    Only post_meeting_check user can modify post_meeting_check

--- app/controllers/post_meeting_checks_controller.rb
+++ app/controllers/post_meeting_checks_controller.rb
@@ -1,4 +1,14 @@
 class PostMeetingChecksController < ApplicationController
+  before_action :current_user_must_be_post_meeting_check_user, :only => [:edit, :update, :destroy]
+
+  def current_user_must_be_post_meeting_check_user
+    post_meeting_check = PostMeetingCheck.find(params[:id])
+
+    unless current_user == post_meeting_check.user
+      redirect_to :back, :alert => "You are not authorized for that."
+    end
+  end
+
   def index
     @post_meeting_checks = PostMeetingCheck.all
 
