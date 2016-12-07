d4e224ab0d0752cb1bd010bab6c7472553fb1112
Raghu Betina <raghu@firstdraft.com>
2016-12-07 16:15:33 +0000

    Only meeting user can access show page

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -1,5 +1,5 @@
 class MeetingsController < ApplicationController
-  before_action :current_user_must_be_meeting_user, :only => [:edit, :update, :destroy]
+  before_action :current_user_must_be_meeting_user, :only => [:show, :edit, :update, :destroy]
 
   def current_user_must_be_meeting_user
     meeting = Meeting.find(params[:id])
