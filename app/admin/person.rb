1a10157368559af9a6eabd82b49056945b9e3b54
Raghu Betina <raghu@firstdraft.com>
2016-12-07 16:15:03 +0000

    Whitelist columns in Person for ActiveAdmin

--- app/admin/person.rb
+++ app/admin/person.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Person do
 
+ permit_params :user_id, :name, :photo, :relationship, :phone, :email, :meeting_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
