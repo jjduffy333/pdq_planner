e03c48a2f20c58171778f04e9d05a44080076050
Raghu Betina <raghu@firstdraft.com>
2016-12-07 16:14:52 +0000

    Whitelist columns in Meeting for ActiveAdmin

--- app/admin/meeting.rb
+++ app/admin/meeting.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Meeting do
 
+ permit_params :user_id, :title, :purpose, :benefit, :check_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
