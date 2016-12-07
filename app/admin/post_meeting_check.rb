face2b6c6c49d7b38fd10d5ec9c923230678cacb
Raghu Betina <raghu@firstdraft.com>
2016-12-07 16:14:57 +0000

    Whitelist columns in PostMeetingCheck for ActiveAdmin

--- app/admin/post_meeting_check.rb
+++ app/admin/post_meeting_check.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register PostMeetingCheck do
 
+ permit_params :check_id, :user_id, :task, :description, :next_check_date
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
